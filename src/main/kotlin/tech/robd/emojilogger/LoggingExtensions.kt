cad568d626d3000c9fc4decdb9c4a1e06065822d
rob <rdeaswork@gmail.com>
2025-04-01 00:04:06 +0100

    add apache licesnce to files

--- src/main/kotlin/tech/robd/emojilogger/LoggingExtensions.kt
+++ src/main/kotlin/tech/robd/emojilogger/LoggingExtensions.kt
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2025 Rob Deas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package tech.robd.emojilogger
 
-
 import kotlinx.coroutines.withContext
-import org.slf4j.*
+import org.slf4j.Logger
+import org.slf4j.MDC
+import org.slf4j.Marker
+import org.slf4j.MarkerFactory
 import kotlin.coroutines.EmptyCoroutineContext
-import kotlinx.coroutines.withContext
 
 /**
  * Top-level emoji logger for fast usage.
